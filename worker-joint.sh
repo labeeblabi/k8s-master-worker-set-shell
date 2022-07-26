6f788ec9bf4b59c1fd196fa2e8800feec67e65a9
labeeblabi <96932991+labeeblabi@users.noreply.github.com>
2022-07-26 15:03:35 +0530

    Update worker-joint.sh

--- worker-joint.sh
+++ worker-joint.sh
@@ -4,9 +4,9 @@ cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
 baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=0
+enabled=1
 gpgcheck=0
-repo_gpgcheck=1
+repo_gpgcheck=0
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
 yum install kubeadm docker -y
