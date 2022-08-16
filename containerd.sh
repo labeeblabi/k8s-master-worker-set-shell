5b3ad1be7a326baf2e6e183ef7070e9980475eb6
labeeblabi <96932991+labeeblabi@users.noreply.github.com>
2022-08-16 12:32:10 +0530

    Create containerd.sh

---
+++ containerd.sh
@@ -0,0 +1,14 @@
+wget https://github.com/containerd/containerd/releases/download/v1.6.2/containerd-1.6.2-linux-amd64.tar.gz
+sudo tar Czxvf /usr/local containerd-1.6.2-linux-amd64.tar.gz
+wget https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
+sudo mv containerd.service /usr/lib/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable --now containerd
+wget https://github.com/opencontainers/runc/releases/download/v1.1.1/runc.amd64
+sudo install -m 755 runc.amd64 /usr/local/sbin/runc
+sudo mkdir -p /opt/cni/bin/
+sudo wget https://github.com/containernetworking/plugins/releases/download/v1.1.1/cni-plugins-linux-amd64-v1.1.1.tgz
+sudo tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.1.1.tgz
+sudo systemctl restart containerd
+wget https://github.com/containerd/nerdctl/releases/download/v0.19.0/nerdctl-0.19.0-linux-amd64.tar.gz
+sudo tar Cxzvf /usr/local/bin nerdctl-0.19.0-linux-amd64.tar.gz
