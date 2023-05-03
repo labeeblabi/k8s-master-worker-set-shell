b0ff76be3b84a3ebae4f3a534ea2f864b1384593
labeeblabi <96932991+labeeblabi@users.noreply.github.com>
2023-05-03 12:42:11 +0530

    Update master-creation.sh

--- master-creation.sh
+++ master-creation.sh
@@ -37,5 +37,4 @@ kubeadm init
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
-export kubever=$(kubectl version | base64 | tr -d '\n')
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$kubever"
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
